74e53233081bf3fd9b169b2c51711ae0434103cd
Zaw Linn <zawlinn20011002@gmail.com>
2023-03-25 16:25:07 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,24 @@
-password = input("Enter password: ")
-while len(password) < 4:
-    print("Password Too Short!")
+MINIMUM_LENGTH = 4
+
+
+def main():
+    """Get and print password."""
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
+
+
+def get_password(minimum_length):
+    """Get password."""
     password = input("Enter password: ")
-print(len(password) * "*")
+    while len(password) < minimum_length:
+        print("Password Too Short!")
+        password = input("Enter password: ")
+    return password
+
+
+def print_asterisks(password):
+    """Print asterisks."""
+    print(len(password) * "*")
+
+
+main()
