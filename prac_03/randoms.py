6b913c676caaa3656edd28ed940d316c26bed1f7
Zaw Linn <zawlinn20011002@gmail.com>
2023-03-27 11:21:22 +0800

    Complete random module exercises

---
+++ prac_03/randoms.py
@@ -0,0 +1,24 @@
+#What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# Number 20 is shown on line 1.
+# The smallest number was 5 and the largest number was 19.
+
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# Number 7 is shown on line 2.
+# The smallest number was 3 and the largest number was 9.
+# No.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# 4.730333025653329 is shown.
+# The smallest number was 2.5 and the largest number was 5.5.
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(1, 101))
