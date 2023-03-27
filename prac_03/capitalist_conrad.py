7254a8c91bc6f2e5a4c4b37ad00582783623784c
Zaw Linn <zawlinn20011002@gmail.com>
2023-03-27 11:42:36 +0800

    Refactor the stock price simulator program

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,17 +9,21 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
-INITIAL_PRICE = 10.0
+MAX_PRICE = 100.0
+INITIAL_PRICE = 1.0
+FILENAME = "stock.txt"
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+day = 0
+print(f"Starting price is: ${price:,.2f}")
 
+out_file = open(FILENAME, 'w')
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    day += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -32,4 +36,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
