eff6dd9a70cc23cd2f5ccbfd9145a5ac9d45a9eb
Zaw Linn <zawlinn20011002@gmail.com>
2023-04-01 00:34:18 +0800

    Add ValueError as exception

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 - Practical
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
