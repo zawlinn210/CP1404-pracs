80338c78d36bba8900eb882b8e0ba3123b471f7a
Zaw Linn <zawlinn20011002@gmail.com>
2023-04-07 13:34:10 +0800

    Refactor subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,11 +9,13 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_subject(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +25,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def display_subject(data):
+    """Display data."""
+    for subjects in data:
+        print(f"{subjects[0]} is taught by {subjects[1]:12} and has {subjects[2]:3} students")
 
 
 main()
