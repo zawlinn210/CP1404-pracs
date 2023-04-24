bf20fe893b84c83ee7f0c368d781ea23c9dabdbd
Zaw Linn <zawlinn20011002@gmail.com>
2023-04-24 11:46:53 +0800

    Complete guitars program exercise

---
+++ prac_06/guitars.py
@@ -0,0 +1,30 @@
+"""
+Code to use Guitar class.
+Estimate: 15 minutes
+Actual:   20 minutes
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+    print("My guitars!")
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "added.")
+        name = input("Name: ")
+
+    print("These are my guitars: ")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
